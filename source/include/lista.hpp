0fc8e6c05290984afa0d05ebe2c870397789dbc0
Jonathan Guzmán <jonguz5@hotmail.com>
2020-08-31 14:49:07 -0600

    convertida lista a plantilla

--- source/include/lista.hpp
+++ source/include/lista.hpp
@@ -1,39 +1,114 @@
 #pragma once
 #include <cstddef>
+#include <stdexcept>
+#include <tuple>
 
-class lista {
-  protected:
+template <typename T> class lista {
+  private:
 	class nodo {
-		int valor;
+		T valor;
 		nodo *siguiente = nullptr;
 
 	  public:
-		nodo(int valor, nodo *siguiente = nullptr)
+		nodo(T valor, nodo *siguiente = nullptr)
 		    : valor(valor), siguiente(siguiente) {}
 		int getValor() { return valor; }
 		void setSiguiente(nodo *n) { siguiente = n; }
 		nodo *getSiguiente() { return siguiente; }
 	};
-
-  private:
 	nodo *inicio = nullptr;
 	size_t len = 0;
 
   public:
-	lista();
-	void insertar(int n, size_t pos = 0);
-	void insertarInicio(int n);
-	void insertarFinal(int n);
-	bool eliminar(size_t pos = 0);
-	bool eliminarInicio();
-	bool eliminarFinal();
+	lista() {}
+	void insertar(int n, size_t pos = 0) {
+		if (pos == 0) {
+			insertarInicio(n);
+		} else if (pos <= len) {
+			nodo *actual = getNodo(pos - 1);
+			actual->setSiguiente(new nodo(n, actual->getSiguiente()));
+			len++;
+		}
+	}
 
-	int getValor(size_t pos);
-	size_t size();
+	void insertarInicio(int n) {
+		inicio = new nodo(n, inicio);
+		len++;
+	}
+	void insertarFinal(int n) {
+		if (len == 0) {
+			insertarInicio(n);
+		} else {
+			getNodo(len - 1)->setSiguiente(new nodo(n));
+			len++;
+		}
+	}
+
+	bool eliminar(size_t pos = 0) {
+		if (pos == 0) {
+			return eliminarInicio();
+		}
+		if (pos < len) {
+			nodo *previo = getNodo(pos - 1);
+			nodo *sig = previo->getSiguiente()->getSiguiente();
+			delete previo->getSiguiente();
+			previo->setSiguiente(sig);
+			len--;
+			return true;
+		}
+		return false;
+	}
+
+	bool eliminarInicio() {
+		if (inicio) {
+			nodo *siguiente = inicio->getSiguiente();
+			delete inicio;
+			inicio = siguiente;
+			len--;
+			return true;
+		}
+		return false;
+	}
+	bool eliminarFinal() {
+		if (len != 0) {
+			if (len == 1) {
+				delete inicio;
+			} else {
+				nodo *n = getNodo(len - 1);
+				delete n->getSiguiente();
+				n->setSiguiente(nullptr);
+			}
+			len--;
+			return true;
+		}
+		return false;
+	}
+
+	int getValor(size_t pos) {
+		nodo *n = getNodo(pos);
+		if (n != nullptr) {
+			return n->getValor();
+		}
+		throw std::out_of_range("posicion fuera de los limites");
+	}
+	size_t size() { return len; }
 
   private:
-	nodo *getNodo(size_t n);
+	lista::nodo *getNodo(size_t pos) {
+		// c++ 17, pre c++17 puede declarar uno de los 2 afuera
+		for (auto [n, i] = std::tuple{inicio, 0}; n != nullptr; i++) {
+			if (i == pos) {
+				return n;
+			}
+			n = n->getSiguiente();
+		}
+		return nullptr;
+	}
 
   public:
-	~lista();
+	~lista() {
+		while (len) {
+			eliminarInicio();
+		}
+	}
 };
