4bd1f3ae8cf474d3a4f5bd6f8b2bfee18fd992c0
Jonathan Guzman <jonguz5@hotmail.com>
2020-09-21 11:12:48 -0600

    solucionados errores de las plantillas

--- source/include/lista.hpp
+++ source/include/lista.hpp
@@ -5,41 +5,42 @@
 
 template <typename T> class lista {
   private:
+	  template <typename A>
 	class nodo {
-		T valor;
+		A valor;
 		nodo *siguiente = nullptr;
 
 	  public:
-		nodo(T valor, nodo *siguiente = nullptr)
+		nodo(A valor, nodo *siguiente = nullptr)
 		    : valor(valor), siguiente(siguiente) {}
-		int getValor() { return valor; }
+		A getValor() { return valor; }
 		void setSiguiente(nodo *n) { siguiente = n; }
 		nodo *getSiguiente() { return siguiente; }
 	};
-	nodo *inicio = nullptr;
+	nodo<T> *inicio = nullptr;
 	size_t len = 0;
 
   public:
 	lista() {}
-	void insertar(int n, size_t pos = 0) {
+	void insertar(T n, size_t pos = 0) {
 		if (pos == 0) {
 			insertarInicio(n);
 		} else if (pos <= len) {
-			nodo *actual = getNodo(pos - 1);
-			actual->setSiguiente(new nodo(n, actual->getSiguiente()));
+			nodo<T> *actual = getNodo(pos - 1);
+			actual->setSiguiente(new nodo<T>(n, actual->getSiguiente()));
 			len++;
 		}
 	}
 
-	void insertarInicio(int n) {
-		inicio = new nodo(n, inicio);
+	void insertarInicio(T n) {
+		inicio = new nodo<T>(n, inicio);
 		len++;
 	}
-	void insertarFinal(int n) {
+	void insertarFinal(T n) {
 		if (len == 0) {
 			insertarInicio(n);
 		} else {
-			getNodo(len - 1)->setSiguiente(new nodo(n));
+			getNodo(len - 1)->setSiguiente(new nodo<T>(n));
 			len++;
 		}
 	}
@@ -49,8 +50,8 @@ template <typename T> class lista {
 			return eliminarInicio();
 		}
 		if (pos < len) {
-			nodo *previo = getNodo(pos - 1);
-			nodo *sig = previo->getSiguiente()->getSiguiente();
+			nodo<T> *previo = getNodo(pos - 1);
+			nodo<T> *sig = previo->getSiguiente()->getSiguiente();
 			delete previo->getSiguiente();
 			previo->setSiguiente(sig);
 			len--;
@@ -61,7 +62,7 @@ template <typename T> class lista {
 
 	bool eliminarInicio() {
 		if (inicio) {
-			nodo *siguiente = inicio->getSiguiente();
+			nodo<T> *siguiente = inicio->getSiguiente();
 			delete inicio;
 			inicio = siguiente;
 			len--;
@@ -74,7 +75,7 @@ template <typename T> class lista {
 			if (len == 1) {
 				delete inicio;
 			} else {
-				nodo *n = getNodo(len - 1);
+				nodo<T> *n = getNodo(len - 1);
 				delete n->getSiguiente();
 				n->setSiguiente(nullptr);
 			}
@@ -84,8 +85,8 @@ template <typename T> class lista {
 		return false;
 	}
 
-	int getValor(size_t pos) {
-		nodo *n = getNodo(pos);
+	T getValor(size_t pos) {
+		nodo<T> *n = getNodo(pos);
 		if (n != nullptr) {
 			return n->getValor();
 		}
@@ -94,7 +95,7 @@ template <typename T> class lista {
 	size_t size() { return len; }
 
   private:
-	lista::nodo *getNodo(size_t pos) {
+	nodo<T> *getNodo(size_t pos) {
 		// c++ 17, pre c++17 puede declarar uno de los 2 afuera
 		for (auto [n, i] = std::tuple{inicio, 0}; n != nullptr; i++) {
 			if (i == pos) {
--- source/include/lista_doble.hpp
+++ source/include/lista_doble.hpp
@@ -3,40 +3,123 @@
 #include <stdexcept>
 #include <tuple>
 
-class listaDoble {
+template <typename T> class listaDoble {
   private:
+	template <typename A>
 	class nodo {
-		int valor;
+		A valor;
 		nodo *siguiente = nullptr;
 		nodo *anterior = nullptr;
 
 	  public:
-		nodo(int valor, nodo *siguiente = nullptr, nodo *anterior = nullptr)
+		
+		nodo(A valor, nodo *siguiente = nullptr, nodo *anterior = nullptr)
 		    : valor(valor), siguiente(siguiente), anterior(anterior) {}
-		int getValor() { return valor; }
+		A getValor() { return valor; }
 		void setSiguiente(nodo *n) { siguiente = n; }
 		void setAnterior(nodo *n) { anterior = n; }
 		nodo *getSiguiente() { return siguiente; }
 		nodo *getAnterior() { return anterior; }
 	};
-	nodo *inicio = nullptr;
-	nodo *ultimo = nullptr;
+	nodo<T> *inicio = nullptr;
+	nodo<T> *ultimo = nullptr;
 	size_t len = 0;
 
   public:
-	listaDoble();
-	void insertar(int n, size_t pos = 0);
-	void insertarInicio(int n);
-	void insertarFinal(int n);
-	bool eliminar(size_t pos = 0);
-	bool eliminarInicio();
-	bool eliminarFinal();
-	int getValor(size_t pos);
-	size_t size();
+	listaDoble() = default;
+	void insertar(T n, size_t pos = 0) {
+		if (pos == 0) {
+			insertarInicio(n);
+		} else if (pos <= len) {
+			nodo<T> *anterior = getNodo(pos - 1);
+			nodo<T> *nuevo = new nodo(n, anterior, anterior->getSiguiente());
+			anterior->getSiguiente()->setSiguiente(nuevo);
+			anterior->setSiguiente(nuevo);
+			len++;
+		}
+	}
+	void insertarInicio(T n) {
+		inicio = new nodo<T>(n, inicio);
+		len++;
+	}
+	void insertarFinal(T n) {
+		if (n == 0) {
+			insertarInicio(n);
+		} else {
+			ultimo->setSiguiente(new nodo(n, nullptr, ultimo));
+			ultimo = ultimo->getSiguiente();
+			// getNodo(len - 1)->setSiguiente(new nodo(n));
+			len++;
+		}
+	}
+	bool eliminar(size_t pos = 0) {
+		if (pos == 0) {
+			return eliminarInicio();
+		}
+		if (pos < len) {
+			nodo<T> *previo = getNodo(pos - 1);
+			nodo<T> *sig = previo->getSiguiente()->getSiguiente();
+			delete previo->getSiguiente();
+			previo->setSiguiente(sig);
+			len--;
+			return true;
+		}
+		return false;
+	}
+	bool eliminarInicio() {
+		if (inicio) {
+			nodo<T> *siguiente = inicio->getSiguiente();
+			delete inicio;
+			if (siguiente) {
+				siguiente->setAnterior(nullptr);
+			}
+			inicio = siguiente;
+			len--;
+			return true;
+		}
+		return false;
+	}
+	bool eliminarFinal() {
+		if (len != 0) {
+			if (len == 1) {
+				delete inicio;
+			} else {
+				nodo<T> *n = getNodo(len - 1);
+				delete n->getSiguiente();
+				n->setSiguiente(nullptr);
+			}
+			len--;
+			return true;
+		}
+		return false;
+	}
+	T getValor(size_t pos) {
+		nodo<T> *n = getNodo(pos);
+		if (n != nullptr) {
+			return n->getValor();
+		}
+		throw std::out_of_range("posicion fuera de los limites");
+	}
+	size_t size() { return len; }
 
   private:
-	nodo *getNodo(size_t pos);
+	nodo<T> *getNodo(size_t pos) {
+		// c++ 17, pre c++17 puede declarar uno de los 2 afuera
+		bool reversa = pos > len / 2;
+		for (auto [n, i] = std::tuple{inicio, reversa ? len - 1 : 0};
+		     n != nullptr; reversa ? i-- : i++) {
+			if (i == pos) {
+				return n;
+			}
+			n = reversa ? n->getAnterior() : n->getSiguiente();
+		}
+		return nullptr;
+	}
 
   public:
-	~listaDoble();
+	~listaDoble() {
+		while (len) {
+			eliminarInicio();
+		}
+	}
 };
