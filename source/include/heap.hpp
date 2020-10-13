efad5c8cc44688b5a09bf4fc5f1a9a5c760d0cbc
Jonathan Guzmán <jonguz5@hotmail.com>
2020-10-12 21:32:46 -0600

    implementacion del heap

--- source/include/heap.hpp
+++ source/include/heap.hpp
@@ -1,3 +1,77 @@
 #pragma once
-#include <lista_doble.hpp>
+#include <vector.hpp>
+
+
+template<typename T>
+class Heap: private Vector<T>
+{
+
+	size_t getPadre(size_t pos) { return (pos - 1) / 2; }
+
+	size_t getIzquierdo(size_t pos) { return (2 * pos + 1); }
+
+
+	size_t getDerecho(size_t pos) { return (2 * pos + 2); }
+
+	void heapify_down(size_t pos) {
+
+		size_t izquierdo = getIzquierdo(pos);
+		size_t derecho = getDerecho(pos);
+		size_t mayor = pos;
+
+
+		if (izquierdo < size() && Vector<T>::at(izquierdo) > Vector<T>::at(pos)) {
+			mayor = izquierdo;
+		}
+			
+		if (derecho < size() && Vector<T>::at(derecho) > Vector<T>::at(mayor)) {
+			mayor = derecho;
+		}
+			
+
+		if (mayor != pos) {
+			Vector<T>::swap(pos, mayor);
+			heapify_down(mayor);
+		}
+	}
+
+	void heapify_up(size_t pos) {
+
+		if (pos && Vector<T>::at(getPadre(pos)) < Vector<T>::at(pos)) {
+			Vector<T>::swap(pos, getPadre(pos));
+			heapify_up(getPadre(pos));
+		}
+	}
+
+public:
+
+	size_t size() { return Vector<T>::size(); }
+
+	bool empty() { return size() == 0; }
+
+	void push(T valor) {
+		Vector<T>::insertarFinal(valor);
+
+		size_t indice = size() - 1;
+		heapify_up(indice);
+	}
+
+
+	void pop() {
+		if (empty()) {
+			throw std::underflow_error("no hay elementos en el Heap");
+		}
+		Vector<T>::at(0) = Vector<T>::at(size() - 1);
+		Vector<T>::eliminarFinal();
+		heapify_down(0);
+	}
+
+
+	T top() {
+		if (empty()) {
+			throw std::underflow_error("no hay elementos en el Heap");
+		}
+		return Vector<T>::at(0);
+	}
+};
 
